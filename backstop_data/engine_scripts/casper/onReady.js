172d7ac9c9ee9cb04f11af97ba0343976f29f98c
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-10-12 17:46:43 +0900

    add noto fonts

--- backstop_data/engine_scripts/casper/onReady.js
+++ backstop_data/engine_scripts/casper/onReady.js
@@ -2,4 +2,7 @@ module.exports = function(casper, scenario, vp) {
   console.log('SCENARIO> ' + scenario.label);
   require('./clickAndHoverHelper')(casper, scenario);
   // add more helpers here...
+  casper.evaluate(function () {
+	return document.querySelector('body').style.fontFamily = '"Noto Sans", "Noto Sans CJK JP", sans-serif';
+  });
 };
